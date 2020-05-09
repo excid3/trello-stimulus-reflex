4e2e4f9c7ebfab06973e5f42e543d786f5d85544
leastbad <hello@leastbad.com>
2020-05-09 07:39:30 -0400

    world peace

--- app/javascript/controllers/board_controller.js
+++ app/javascript/controllers/board_controller.js
@@ -1,18 +1,23 @@
-import SortableController from "controllers/sortable_controller"
+import SortableController from 'controllers/sortable_controller'
 
 export default class extends SortableController {
-  sorted(event) {
+  sorted (event) {
     this.lastId = Math.random()
-    this.stimulate("ListReflex#move", event.item, {
+    this.stimulate('ListReflex#move', event.item, {
       position: event.newIndex + 1,
       forceUpdateId: this.lastId
     })
   }
 
-  create_list(event) {
+  create_list (event) {
     event.preventDefault()
     let formData = new FormData(event.target)
     let data = Object.fromEntries(formData.entries())
-    this.stimulate("ListReflex#create", data)
+    this.stimulate('ListReflex#create', data)
+  }
+
+  reload (event) {
+    event.preventDefault()
+    this.stimulate('ListReflex#reload')
   }
 }
